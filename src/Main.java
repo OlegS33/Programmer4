eed5b1713b7fb7ec2a81aae1bf0a440e09c4c57e
OlegS33 <soldatov.2000@yandex.ru>
2023-01-09 16:59:52 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,10 @@ public class Main {
         System.out.println(Slava.getPosition());
         Slava.work();
         System.out.println(Slava.getPosition());
+        Slava.work();
+        System.out.println(Slava.getPosition());
+        Slava.work();
+        System.out.println(Slava.getPosition());
 
     }
 }
